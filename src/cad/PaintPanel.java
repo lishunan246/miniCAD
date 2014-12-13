231ad17732f28ee6f6a6ea61c6ef7542a0052689
Li Shunan <lishunan246@gmail.com>
2014-12-13 13:31:48 +0800

    add ArrayList

--- src/cad/PaintPanel.java
+++ src/cad/PaintPanel.java
@@ -2,16 +2,23 @@ package cad;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by lishunan on 14-12-13.
  */
 public class PaintPanel extends JPanel {
+    ArrayList <Target> arrayList=new ArrayList<Target>();
     protected void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         Target target=new Target(g);
-        target.draw();
+        arrayList.add(target);
+
+        for(Target target1:arrayList)
+        {
+            target1.draw();
+        }
 
         g.drawString("bannwe",0,40);
     }
